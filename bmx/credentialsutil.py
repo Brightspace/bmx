8010767f2f936ffbe6028fc99bedd5a4fe9086a9
Chris Redekop <chris.redekop@d2l.com>
2017-09-18 13:25:01 -0400

    Consider account and role when fetching credentials

--- bmx/credentialsutil.py
+++ bmx/credentialsutil.py
@@ -22,17 +22,22 @@ def get_credentials_path():
 def get_cookie_session_path():
     return os.path.join(get_bmx_path(), 'cookies.state')
 
-def read_credentials():
+def read_credentials(app=None, role=None):
+    if not app and role or app and not role:
+        return None
+
     if os.path.exists(get_credentials_path()):
         with open(get_credentials_path(), 'r') as credentials_file:
-            credentials_object = yaml.load(credentials_file) or {}
-            credentials_dict = credentials_object.get(META_KEY, {})
+            credentials_doc = yaml.load(credentials_file) or {}
 
-            #TODO: Get account and role from credentials file
-            return AwsCredentials(
-                credentials_dict.get(DEFAULT_KEY),
-                'get_account_from_file',
-                'get_role_from_file')
+        if not app:
+            default_ref = credentials_doc.get(META_KEY, {}).get(DEFAULT_KEY, {})
+            app = default_ref.get(app, None)
+            role = default_ref.get(role, None)
+
+        credentials = credentials_doc.get(CREDENTIALS_KEY, {}).get(app, {}).get(role, None)
+
+        return AwsCredentials(credentials, app, role) if credentials else None
 
 def write_credentials(credentials):
     create_bmx_path()
@@ -55,7 +60,7 @@ def write_credentials(credentials):
 
 
 def fetch_credentials(username=None, duration_seconds=3600, app=None, role=None):
-    return read_credentials() or stsutil.get_credentials(
+    return read_credentials(app, role) or stsutil.get_credentials(
         username,
         duration_seconds,
         app,
