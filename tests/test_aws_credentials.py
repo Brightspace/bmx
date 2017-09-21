2788f945e4197fa0de1199f5344f726621bf15a7
Miles Tjandrawidjaja <miles.tjandrawidjaja@d2l.com>
2017-09-21 17:19:59 -0400

    Full coverage of AwsCredentials

--- tests/test_aws_credentials.py
+++ tests/test_aws_credentials.py
@@ -1,16 +1,16 @@
 import unittest
 from datetime import datetime, timedelta, timezone
 
-from .context import bmx
 from bmx.aws_credentials import AwsCredentials
+from bmx.constants import AWS_ACCOUNT_KEY, AWS_ROLE_KEY
 
 class AwsCredentialsTest(unittest.TestCase):
-    def test_normalize_role_with_substituion(self):
+    def test_normalize_role_with_substitution(self):
         expected_role_name = 'expectedRole'
         role_name = AwsCredentials.extract_role_name(f'arn:aws:iam::123:role/{expected_role_name}')
         self.assertEqual(expected_role_name, role_name)
 
-    def test_normalize_role_without_substituion(self):
+    def test_normalize_role_without_substitution(self):
         expected_role_name = 'expectedRole'
         role_name = AwsCredentials.extract_role_name(expected_role_name)
         self.assertEqual(expected_role_name, role_name)
@@ -30,13 +30,15 @@ class AwsCredentialsTest(unittest.TestCase):
             AwsCredentials.normalize_keys({'Expiration': expected_expiration})
         self.assertEqual(expected_expiration, normalized_keys['Expiration'])
 
-    def test_are_expired_returns_true_when_expiration_is_in_the_past(self):
-        self.assertTrue(AwsCredentials(
-            {
-                'Expiration': datetime.now(timezone.utc) - timedelta(days=1)
-            },
-            'expected_account',
-            'expected_role').have_expired())
+
+    def test_are_expired_returns_true_when_expiration_is_in_the_past_or_equal(self):
+        for day in [1, 0]:
+            self.assertTrue(AwsCredentials(
+                {
+                    'Expiration': datetime.now(timezone.utc) - timedelta(days=day)
+                },
+                'expected_account',
+                'expected_role').have_expired())
 
     def test_are_expired_returns_false_when_expiration_is_in_the_future(self):
         self.assertFalse(AwsCredentials(
@@ -45,3 +47,13 @@ class AwsCredentialsTest(unittest.TestCase):
             },
             'expected_account',
             'expected_role').have_expired())
+
+    def test_get_principal_dict(self):
+        expected_account = 'expected_account'
+        expected_role = 'expected_role'
+        aws_credentials = AwsCredentials({}, expected_account, expected_role)
+        expected_principal = {
+            AWS_ACCOUNT_KEY: expected_account,
+            AWS_ROLE_KEY: expected_role
+        }
+        self.assertDictEqual(expected_principal, aws_credentials.get_principal_dict())
