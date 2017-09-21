5d9e4808c9af1b7a6e5b50d405601d2f86f2152e
Miles Tjandrawidjaja <miles.tjandrawidjaja@d2l.com>
2017-09-21 14:27:37 -0400

    Test parser for bmxwrite - be explicit about defaults

--- tests/test_bmxwrite.py
+++ tests/test_bmxwrite.py
@@ -1,120 +1,24 @@
-import base64
-import contextlib
-import io
-import json
-import os
 import unittest
 
-from unittest.mock import Mock, MagicMock
 from unittest.mock import patch
 
-from .context import bmx
-import bmx.bmxprint as bmxprint
 import bmx.bmxwrite as bmxwrite
-import okta.models.user
-import bmx.prompt as prompt
-import bmx.credentialsutil as credentialsutil
-from bmx.aws_credentials import AwsCredentials
+from bmx.locale.options import (BMX_WRITE_USAGE, BMX_WRITE_PROFILE_HELP,
+                                BMX_ACCOUNT_HELP, BMX_ROLE_HELP, BMX_USERNAME_HELP)
 
-class MockSession():
-    def __init__(self):
-        pass
-    def userId(self):
-        return ''
-    def validate_session(self, x):
-        return x
 
 class BmxWriteTests(unittest.TestCase):
-    @patch('argparse.ArgumentParser')
-    def test_create_parser_should_create_expected_parser_always(self, mock_arg_parser):
+    @patch('argparse.ArgumentParser', autospec=True)
+    def test_create_parser_should_create_expected_parser_always(self, mock_argparser_constructor):
         parser = bmxwrite.create_parser()
 
-        calls = parser.add_argument.call_args_list
-        self.assertEqual(4, len(calls))
 
-        self.assertEqual('--username', calls[0][0][0])
-        self.assertTrue('help' in calls[0][1])
+        mock_argparser_constructor.assert_called_once_with(prog='bmx write', usage=BMX_WRITE_USAGE)
+        parser.add_argument.assert_any_call('--username', default=None, help=BMX_USERNAME_HELP)
+        parser.add_argument.assert_any_call('--profile', default='default', help=BMX_WRITE_PROFILE_HELP)
+        parser.add_argument.assert_any_call('--account', default=None, help=BMX_ACCOUNT_HELP)
+        parser.add_argument.assert_any_call('--role', default=None, help=BMX_ROLE_HELP)
 
-        self.assertEqual('--profile', calls[1][0][0])
-        self.assertTrue('help' in calls[1][1])
-
-        self.assertEqual('--account', calls[2][0][0])
-        self.assertTrue('help' in calls[2][1])
-
-        self.assertEqual('--role', calls[3][0][0])
-        self.assertTrue('help' in calls[3][1])
-
-
-    #@patch('builtins.open')
-    #@patch('boto3.client')
-    #@patch('configparser.ConfigParser', return_value=MagicMock())
-    #@patch('bmx.stsutil.get_app_roles')
-    #@patch('bmx.stsutil.oktautil')
-    #@patch('bmx.prompt.prompt_for_value', return_value="password")
-    #def test_write_with_account_and_role_should_write_correct_data(self,
-                                                                   #mock_prompt,
-                                                                   #mock_oktautil,
-                                                                   #mock_get_app_roles,
-                                                                   #mock_configparser,
-                                                                   #mock_boto3,
-                                                                   #mock_open):
-        #def getAppLink(props):
-            #applink = okta.models.user.AppLinks()
-            #for key, value in props.items():
-                #applink.__setattr__(key, value)
-            #return applink
-
-
-        #mock_oktautil.get_okta_session.return_value = MockSession(), 'someCookieObject'
-
-        #mock_oktautil.create_users_client.return_value.get_user_applinks.return_value = [
-            #getAppLink({'appName': 'amazon_aws', 'label': 'not-my-account'}),
-            #getAppLink({'appName': 'amazon_aws', 'label': 'my-account'})
-        #]
-#
-        #mock_oktautil.connect_to_app.return_value = base64.b64encode(b'skip_me')
-
-        #mock_get_app_roles.return_value = [
-            #'arn:aws:iam::accountid:saml-provider/Okta,arn:aws:iam::accountid:role/my-role',
-            #'arn:aws:iam::accountid:saml-provider/Okta,arn:aws:iam::accountid:role/not-my-role'
-        #]
-
-        #mock_boto3.return_value.assume_role_with_saml.return_value = {'Credentials':
-                                                                          #{'AccessKeyId': 'my-access-key',
-                                                                           #'SecretAccessKey': 'my-secret-access-key',
-                                                                           #'SessionToken': 'my-session-token'}
-                                                                      #}
-
-        #bmxwrite.cmd(['--profile', 'my-profile',
-                      #'--account', 'my-account',
-                      #'--username', 'my-user',
-                      #'--role', 'my-role'])
-
-        #mock_configparser.return_value.__setitem__.assert_called_with('my-profile', {'aws_access_key_id': 'my-access-key',
-                                                                                    #'aws_secret_access_key': 'my-secret-access-key',
-                                                                                    #'aws_session_token': 'my-session-token'})
-        #assert mock_configparser.return_value.write.called
-        #mock_open.assert_called_with(os.path.expanduser('~/.aws/credentials'), 'w')
-
-    #@patch('bmx.bmxprint.create_parser')
-    #def test_cmd_should_print_credentials_always(self, mock_arg_parser):
-        #return_value = {
-            #'AccessKeyId': ACCESS_KEY_ID,
-            #'SecretAccessKey': SECRET_ACCESS_KEY,
-            #'SessionToken': SESSION_TOKEN
-        #}
-
-        #bmx.bmxwrite.get_credentials = Mock(
-            #return_value=return_value
-        #)
-
-        #out = io.StringIO()
-        #with contextlib.redirect_stdout(out):
-            #self.assertEqual(0, bmx.bmxprint.cmd([]))
-        #out.seek(0)
-        #printed = json.load(out)
-
-        #self.assertEqual(return_value, printed)
 
 if __name__ == '__main__':
-    unittest.main();
+    unittest.main()
