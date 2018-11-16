890b4935d413d16cb19586edefcea66405a0c622
Andrew Alkema <aalkema@gmail.com>
2018-11-15 22:10:56 -0500

    Update okta_test.go
    Fix issue

--- okta/okta_test.go
+++ okta/okta_test.go
@@ -4,37 +4,29 @@ import (
 	"testing"
 
 	"github.com/Brightspace/bmx/okta"
+	"github.com/Brightspace/bmx/okta/file"
 	"github.com/Brightspace/bmx/okta/mocks"
 )
 
-func TestOkta_GetSamlWithNoExistingSession(t *testing.T) {
-	client, err := okta.NewOktaClient("test")
-	client.SessionCache = mocks.DefaultSessionCache()
+type OktaClient struct {
+	*okta.OktaClient
+	SessionCache mocks.SessionCache
+}
 
-	c := MustOpenClient()
-	defer c.Close()
-	s := c.Connect().DialService()
-	// Mock authentication.
-	c.Authenticator.AuthenticateFn = func(_ string) (*wtf.User, error) {
-		return &wtf.User{ID: 123}, nil
+func NewOktaClient() *OktaClient {
+	client, _ := okta.NewOktaClient("test")
+	o := &OktaClient{
+		OktaClient:   client,
+		SessionCache: mocks.DefaultSessionCache(),
 	}
-	dial := wtf.Dial{
-		Name:  "MY DIAL",
-		Level: 50,
-	}
-	// Create new dial.
-	if err := s.CreateDial(&dial); err != nil {
-		t.Fatal(err)
-	} else if dial.ID != 1 {
-		t.Fatalf("unexpected id: %d", dial.ID)
-	} else if dial.UserID != 123 {
-		t.Fatalf("unexpected user id: %d", dial.UserID)
-	}
-	// Retrieve dial and compare.
-	other, err := s.Dial(1)
-	if err != nil {
-		t.Fatal(err)
-	} else if !reflect.DeepEqual(&dial, other) {
-		t.Fatalf("unexpected dial: %#v", other)
+	o.OktaClient.SessionCache = &o.SessionCache
+
+	return o
+}
+
+func TestOkta_GetSamlWithNoExistingSession(t *testing.T) {
+	client := NewOktaClient()
+	client.SessionCache.SessionsFn = func() ([]file.OktaSessionCache, error) {
+		return nil, nil
 	}
 }
