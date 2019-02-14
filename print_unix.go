1d566d85342c658cc97b92da08f3e3990ea0ac37
Chris Stavropoulos <chris.stavropoulos@d2l.com>
2019-02-14 14:33:35 -0500

    fix go vet failure

--- print_unix.go
+++ print_unix.go
@@ -1,3 +1,5 @@
+// +build linux darwin freebsd netbsd openbsd
+
 /*
 Copyright 2019 D2L Corporation
 
@@ -13,8 +15,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
-// +build linux darwin freebsd netbsd openbsd
 
 package bmx
 
