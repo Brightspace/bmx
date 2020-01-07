656966c78023c4506134983a106630302bef8f0c
jrbeverly <Jonathan.Beverly@D2L.com>
2020-01-07 16:15:37 -0500

    Test expected output from custom command

--- print_test.go
+++ print_test.go
@@ -17,6 +17,10 @@ limitations under the License.
 package bmx_test
 
 import (
+	"bytes"
+	"io"
+	"os"
+	"strings"
 	"testing"
 
 	"github.com/Brightspace/bmx"
@@ -33,4 +37,72 @@ func TestMonkey(t *testing.T) {
 	bmx.AwsServiceProvider = &mocks.AwsServiceProviderMock{}
 	bmx.ConsoleReader = mocks.ConsoleReaderMock{}
 	bmx.Print(oktaClient, options)
+}
+
+func testBmxPrint(oktaClient *mocks.Mokta, options bmx.PrintCmdOptions) (string, error) {
+	stdout := os.Stdout
+
+	read, write, err := os.Pipe()
+	if err != nil {
+		return "", err
+	}
+	os.Stdout = write
+
+	// Run bmx print and capture output
+	bmx.Print(oktaClient, options)
+
+	stdoutChan := make(chan string)
+	go func() {
+		var resp bytes.Buffer
+		io.Copy(&resp, read)
+		stdoutChan <- resp.String()
+	}()
+
+	write.Close()
+	os.Stdout = stdout
+	result := <-stdoutChan
+
+	return result, nil
+}
+
+func TestPShellPrint(t *testing.T) {
+	options := bmx.PrintCmdOptions{
+		Org:    "myorg",
+		Output: bmx.Powershell,
+	}
+
+	oktaClient := &mocks.Mokta{}
+
+	bmx.AwsServiceProvider = &mocks.AwsServiceProviderMock{}
+	bmx.ConsoleReader = mocks.ConsoleReaderMock{}
+
+	output, err := testBmxPrint(oktaClient, options)
+	if err != nil {
+		t.Errorf("Encountered exception while running bmx print, got: %s", err)
+	}
+
+	if !strings.Contains(output, "$env:") {
+		t.Errorf("Shell command was incorrect, got: %s, expected powershell", output)
+	}
+}
+
+func TestBashPrint(t *testing.T) {
+	options := bmx.PrintCmdOptions{
+		Org:    "myorg",
+		Output: bmx.Bash,
+	}
+
+	oktaClient := &mocks.Mokta{}
+
+	bmx.AwsServiceProvider = &mocks.AwsServiceProviderMock{}
+	bmx.ConsoleReader = mocks.ConsoleReaderMock{}
+
+	output, err := testBmxPrint(oktaClient, options)
+	if err != nil {
+		t.Errorf("Encountered exception while running bmx print, got: %s", err)
+	}
+	
+	if !strings.Contains(output, "export ") {
+		t.Errorf("Shell command was incorrect, got: %s, expected bash", output)
+	}
 }
