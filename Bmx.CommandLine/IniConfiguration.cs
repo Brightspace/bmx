c1beee9530f93e25bc8db28dbad6cbfff753139e
Adipa Wijayathilaka <adipamihi@gmail.com>
2020-07-16 09:04:51 -0400

    Fixed .bmx file not being read, made .bmx/config optional. Removed ShouldReadProjectConfigs for clarity.

--- Bmx.CommandLine/IniConfiguration.cs
+++ Bmx.CommandLine/IniConfiguration.cs
@@ -3,6 +3,8 @@ using System;
 using System.IO;
 using System.Security;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.FileProviders.Physical;
 
 namespace Bmx.CommandLine {
 	public class IniConfiguration : IBmxConfig {
@@ -24,31 +26,28 @@ namespace Bmx.CommandLine {
 
 		private static IConfiguration GetConfiguration() {
 			// Main config is at ~/.bmx/config
-			var configLocation =
-				Path.Join( Environment.GetFolderPath( Environment.SpecialFolder.UserProfile ), ".bmx", "config" );
+			var configLocation = Path.Join( Environment.GetFolderPath( Environment.SpecialFolder.UserProfile ), ".bmx",
+				"config" );
 
 			// If set, we recursively look up from CWD (all the way to root) for additional bmx config files (labelled as .bmx)
-			var shouldReadProjectConfig = ShouldReadProjectConfigs( configLocation );
+			var config = new ConfigurationBuilder().AddIniFile( configLocation, optional: true );
+
+			bool.TryParse( config.Build()["allow_project_configs"], out bool shouldReadProjectConfig );
+
 			string? projectConfigPath = null;
 
 			if( shouldReadProjectConfig ) {
 				projectConfigPath = GetProjectConfigPath();
 			}
 
-			var config = new ConfigurationBuilder().AddIniFile( configLocation );
-
 			if( shouldReadProjectConfig && projectConfigPath != null ) {
-				config.AddIniFile( projectConfigPath );
+				// Default file provider ignores files prefixed with ".", we need to provide our own as a result
+				var fileProvider =
+					new PhysicalFileProvider( Path.GetDirectoryName( projectConfigPath ), ExclusionFilters.None );
+				config.AddIniFile( fileProvider, Path.GetFileName( projectConfigPath ), optional: false, reloadOnChange: false );
 			}
 
 			return config.Build();
-		}
-
-		private static bool ShouldReadProjectConfigs( string primaryConfigLocation ) {
-			var config = new ConfigurationBuilder()
-				.AddIniFile( primaryConfigLocation ).Build();
-			bool.TryParse( config["allow_project_configs"], out bool allowProjectConfigs );
-			return allowProjectConfigs;
 		}
 
 		// Look from cwd up the directory chain all the way to root for a .bmx file
