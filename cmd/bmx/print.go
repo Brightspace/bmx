1ff94f3c22382c1dfe31df0f1215a7eb6d2586ad
Kevin Tonon <kevin.tonon@d2l.com>
2022-05-17 12:45:11 -0400

    Adding default_duration to config

--- cmd/bmx/print.go
+++ cmd/bmx/print.go
@@ -35,7 +35,7 @@ func init() {
 	printCmd.Flags().StringVar(&printOptions.User, "user", "", "the user to authenticate with")
 	printCmd.Flags().StringVar(&printOptions.Account, "account", "", "the account name to auth against")
 	printCmd.Flags().StringVar(&printOptions.Role, "role", "", "the desired role to assume")
-	printCmd.Flags().IntVar(&printOptions.Duration, "duration", 60, "duration of session in minutes")
+	printCmd.Flags().IntVar(&printOptions.Duration, "duration", userConfig.DefaultDuration, "duration of session in minutes")
 	printCmd.Flags().BoolVar(&printOptions.NoMask, "nomask", false, "set to not mask the password. this helps with debugging.")
 	printCmd.Flags().StringVar(&printOptions.Output, "output", "", "the output format [bash|powershell]")
 
