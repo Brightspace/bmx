7c81bcc9819af8a486a627771ae5c55caf6ad4e9
Johann Cardenas <Johann.Cardenas@D2L.com>
2023-03-16 14:51:24 -0400

    making role state a record

--- src/D2L.Bmx/Aws/AwsRoleState.cs
+++ src/D2L.Bmx/Aws/AwsRoleState.cs
@@ -1,13 +1,6 @@
 namespace D2L.Bmx.Aws;
 
-internal class AwsRoleState {
-	public string?[] Roles { get; }
-	internal List<AwsRole> AwsRoles { get; }
-	internal string SamlString { get; }
-
-	public AwsRoleState( List<AwsRole> awsRoles, string samlString ) {
-		AwsRoles = awsRoles;
-		SamlString = samlString;
-		Roles = AwsRoles.Select( role => role.RoleName ).ToArray();
-	}
-}
+internal record AwsRoleState(
+	List<AwsRole> AwsRoles,
+	string SamlString
+);
