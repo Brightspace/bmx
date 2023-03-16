3dc806d20c2f454d25741919f849b2d3618e14a1
Johann Cardenas <Johann.Cardenas@D2L.com>
2023-03-16 13:44:53 -0400

    fixing linting errors

--- src/D2L.Bmx/Okta/Models/AuthenticateChallengeMfaOptions.cs
+++ src/D2L.Bmx/Okta/Models/AuthenticateChallengeMfaOptions.cs
@@ -2,7 +2,7 @@ using D2L.Bmx.Okta;
 namespace D2L.Bmx.Okta.Models;
 
 internal record AuthenticateChallengeMfaOptions(
-	 string FactorId,
-	 string PassCode,
-	 string StateToken
+	string FactorId,
+	string PassCode,
+	string StateToken
 );
