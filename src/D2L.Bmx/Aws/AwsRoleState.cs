cca00dbf5ebc3bfcc402192e490c22f4de6c6f2e
Johann Cardenas <Johann.Cardenas@D2L.com>
2023-03-17 15:27:09 -0400

    fixing role state

--- src/D2L.Bmx/Aws/AwsRoleState.cs
+++ src/D2L.Bmx/Aws/AwsRoleState.cs
@@ -1,13 +1,6 @@
 namespace D2L.Bmx.Aws;
 
-internal class AwsRoleState {
-	public string?[] Roles { get; }
-	public List<AwsRole> AwsRoles { get; }
-	public string SamlString { get; }
-
-	public AwsRoleState( List<AwsRole> awsRoles, string samlString ) {
-		AwsRoles = awsRoles;
-		SamlString = samlString;
-		Roles = AwsRoles.Select( role => role.RoleName ).ToArray();
-	}
-}
+internal record AwsRoleState(
+	List<AwsRole> AwsRoles,
+	string SamlString
+);
