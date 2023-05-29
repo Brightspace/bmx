6f47fae99f57115c6ea477450d767cebfb2b9868
Chenfeng Bao <chenfeng@chenfengbao.com>
2023-05-29 14:11:33 -0400

    work around .NET's bug on console input + stdout on Linux & Mac

--- src/D2L.Bmx/ConsolePrompter.cs
+++ src/D2L.Bmx/ConsolePrompter.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 using D2L.Bmx.Okta.Models;
 
 namespace D2L.Bmx;
@@ -16,10 +18,20 @@ internal interface IConsolePrompter {
 }
 
 internal class ConsolePrompter : IConsolePrompter {
+	// When taking user console input on Unix-y platforms, .NET copies the input data to stdout, leading to incorrect
+	// `bmx print` output. See https://github.com/dotnet/runtime/issues/22314.
+	// We read from stdin (fd = 0) directly on these platforms to bypass .NET's incorrect handling.
+	private readonly TextReader _stdinReader =
+		RuntimeInformation.IsOSPlatform( OSPlatform.Windows )
+		? Console.In
+		: new StreamReader( new FileStream(
+			new Microsoft.Win32.SafeHandles.SafeFileHandle( 0, ownsHandle: false ),
+			FileAccess.Read
+		) );
 
 	string IConsolePrompter.PromptOrg() {
 		Console.Error.Write( "The tenant name or full domain name of the Okta organization: " );
-		string? org = Console.ReadLine();
+		string? org = _stdinReader.ReadLine();
 		if( string.IsNullOrEmpty( org ) ) {
 			throw new BmxException( "Invalid org input" );
 		}
@@ -29,7 +41,7 @@ internal class ConsolePrompter : IConsolePrompter {
 
 	string IConsolePrompter.PromptProfile() {
 		Console.Error.Write( "AWS profile: " );
-		string? profile = Console.ReadLine();
+		string? profile = _stdinReader.ReadLine();
 		if( string.IsNullOrEmpty( profile ) ) {
 			throw new BmxException( "Invalid profile input" );
 		}
@@ -39,7 +51,7 @@ internal class ConsolePrompter : IConsolePrompter {
 
 	string IConsolePrompter.PromptUser() {
 		Console.Error.Write( "Okta Username: " );
-		string? user = Console.ReadLine();
+		string? user = _stdinReader.ReadLine();
 		if( string.IsNullOrEmpty( user ) ) {
 			throw new BmxException( "Invalid user input" );
 		}
@@ -49,28 +61,42 @@ internal class ConsolePrompter : IConsolePrompter {
 
 	string IConsolePrompter.PromptPassword() {
 		Console.Error.Write( "Okta Password: " );
-		var passwordChars = new List<char>();
 
-		while( true ) {
-			ConsoleKeyInfo input = Console.ReadKey( intercept: true );
-			if( input.Key == ConsoleKey.Enter ) {
-				Console.Error.Write( '\n' );
-				break;
-			}
+		if( RuntimeInformation.IsOSPlatform( OSPlatform.Windows ) ) {
+			var passwordChars = new List<char>();
+			while( true ) {
+				ConsoleKeyInfo input = Console.ReadKey( intercept: true );
+				if( input.Key == ConsoleKey.Enter ) {
+					Console.Error.Write( '\n' );
+					break;
+				}
 
-			if( input.Key == ConsoleKey.Backspace && passwordChars.Count > 0 ) {
-				passwordChars.RemoveAt( passwordChars.Count - 1 );
-			} else if( !char.IsControl( input.KeyChar ) ) {
-				passwordChars.Add( input.KeyChar );
+				if( input.Key == ConsoleKey.Backspace && passwordChars.Count > 0 ) {
+					passwordChars.RemoveAt( passwordChars.Count - 1 );
+				} else if( !char.IsControl( input.KeyChar ) ) {
+					passwordChars.Add( input.KeyChar );
+				}
 			}
+			return new string( passwordChars.ToArray() );
 		}
 
-		return new string( passwordChars.ToArray() );
+		string? password;
+		try {
+			// disable the terminal from echoing user input
+			using var stty = Process.Start( "stty", "-echo" );
+			stty.WaitForExit();
+			password = _stdinReader.ReadLine();
+		} finally {
+			using var stty = Process.Start( "stty", "echo" );
+			stty.WaitForExit();
+		}
+		Console.Error.WriteLine();
+		return password ?? throw new BmxException( "No password entered" );
 	}
 
 	int? IConsolePrompter.PromptDefaultDuration() {
 		Console.Error.Write( "Default duration of AWS sessions in minutes (optional, default: 60): " );
-		string? input = Console.ReadLine();
+		string? input = _stdinReader.ReadLine();
 		if( input is null || !int.TryParse( input, out int defaultDuration ) || defaultDuration <= 0 ) {
 			return null;
 		}
@@ -79,7 +105,7 @@ internal class ConsolePrompter : IConsolePrompter {
 
 	bool IConsolePrompter.PromptAllowProjectConfig() {
 		Console.Error.Write( "Allow project level .bmx config file? (true/false, default: false): " );
-		string? input = Console.ReadLine();
+		string? input = _stdinReader.ReadLine();
 		if( input is null || !bool.TryParse( input, out bool allowProjectConfigs ) ) {
 			return false;
 		}
@@ -92,7 +118,7 @@ internal class ConsolePrompter : IConsolePrompter {
 			Console.Error.WriteLine( $"[{i + 1}] {accounts[i]}" );
 		}
 		Console.Error.Write( "Select an account: " );
-		if( !int.TryParse( Console.ReadLine(), out int index ) || index > accounts.Length || index < 1 ) {
+		if( !int.TryParse( _stdinReader.ReadLine(), out int index ) || index > accounts.Length || index < 1 ) {
 			throw new BmxException( "Invalid account selection" );
 		}
 
@@ -105,7 +131,7 @@ internal class ConsolePrompter : IConsolePrompter {
 			Console.Error.WriteLine( $"[{i + 1}] {roles[i]}" );
 		}
 		Console.Error.Write( "Select a role: " );
-		if( !int.TryParse( Console.ReadLine(), out int index ) || index > roles.Length || index < 1 ) {
+		if( !int.TryParse( _stdinReader.ReadLine(), out int index ) || index > roles.Length || index < 1 ) {
 			throw new BmxException( "Invalid role selection" );
 		}
 
@@ -128,7 +154,7 @@ internal class ConsolePrompter : IConsolePrompter {
 			Console.Error.WriteLine( $"[{i + 1}] {mfaOptions[i].Provider}: {mfaOptions[i].FactorType}" );
 		}
 		Console.Error.Write( "Select an available MFA option: " );
-		if( !int.TryParse( Console.ReadLine(), out int index ) || index > mfaOptions.Length || index < 1 ) {
+		if( !int.TryParse( _stdinReader.ReadLine(), out int index ) || index > mfaOptions.Length || index < 1 ) {
 			throw new BmxException( "Invalid account selection" );
 		}
 		return mfaOptions[index - 1];
@@ -136,7 +162,7 @@ internal class ConsolePrompter : IConsolePrompter {
 
 	string IConsolePrompter.GetMfaResponse( string mfaInputPrompt ) {
 		Console.Error.Write( $"{mfaInputPrompt}: " );
-		string? mfaInput = Console.ReadLine();
+		string? mfaInput = _stdinReader.ReadLine();
 
 		if( mfaInput is not null ) {
 			return mfaInput;
