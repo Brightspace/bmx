da0e171ddaaa17a2437f2ed5b999a26e6e750c64
Liam Gordon <liam.gordon@d2l.com>
2023-06-23 07:42:23 -0400

    try something

--- src/D2L.Bmx/UnixParentProcess.cs
+++ src/D2L.Bmx/UnixParentProcess.cs
@@ -7,10 +7,12 @@ class UnixParentProcess {
 	public static string GetParentProcessName() {
 		var parentPid = GetParentProcessPid();
 		Console.WriteLine( $"pid is {parentPid}" );
+		var xParentPid = GetXParentProcessPid( parentPid );
+		Console.WriteLine( $"{xParentPid}" );
 		Process proc = new Process {
 			StartInfo = new ProcessStartInfo {
 				FileName = "/bin/bash",
-				Arguments = $"-c \"ps -p {parentPid} -o comm=\"",
+				Arguments = $"-c \"ps -p {xParentPid} -o comm=\"",
 				RedirectStandardOutput = true,
 				UseShellExecute = false,
 				CreateNoWindow = true
@@ -21,6 +23,22 @@ class UnixParentProcess {
 		string parentProcName = proc.StandardOutput.ReadToEnd().Trim();
 		proc.WaitForExit();
 		return parentProcName;
+	}
+	private static string GetXParentProcessPid( string pid ) {
+		Process proc = new Process {
+			StartInfo = new ProcessStartInfo {
+				FileName = "/bin/bash",
+				Arguments = $"-c \"ps -p {pid} -o ppid=\"",
+				RedirectStandardOutput = true,
+				UseShellExecute = false,
+				CreateNoWindow = true
+			}
+		};
+
+		proc.Start();
+		string parentPid = proc.StandardOutput.ReadToEnd().Trim();
+		proc.WaitForExit();
+		return parentPid;
 	}
 
 	private static string GetParentProcessPid() {
