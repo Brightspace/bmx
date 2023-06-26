1c966129bcf16df49527d37144b4d847378d4067
Liam Gordon <liam.gordon@d2l.com>
2023-06-26 12:22:23 -0400

    use built in function for retrieving process name

--- src/D2L.Bmx/UnixParentProcess.cs
+++ src/D2L.Bmx/UnixParentProcess.cs
@@ -6,22 +6,7 @@ internal class UnixParentProcess {
 
 	public static string GetParentProcessName() {
 		int parentPid = GetParentProcessId( GetParentProcessId( Process.GetCurrentProcess().Id ) );
-		var proccessStartInfo = new ProcessStartInfo {
-			FileName = "/bin/ps",
-			ArgumentList = { "-p", $"{parentPid}", "-o", "comm=" },
-			RedirectStandardOutput = true,
-			UseShellExecute = false,
-			CreateNoWindow = true
-		};
-
-		using( var proc = Process.Start( proccessStartInfo ) ) {
-			if( proc is not null ) {
-				string output = proc.StandardOutput.ReadToEnd().Replace( "-", "" );
-				proc.WaitForExit();
-				return output;
-			}
-			return "bash";
-		}
+		return Process.GetProcessById( parentPid ).ProcessName;
 	}
 
 	private static int GetParentProcessId( int pid ) {
