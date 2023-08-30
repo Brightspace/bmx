0219a986c2c125297b81623901a52b8f740a3f3d
Liam Gordon <liam.gordon@d2l.com>
2023-08-30 09:30:17 -0400

    remvoe imports

--- src/D2L.Bmx/UpdateChecker.cs
+++ src/D2L.Bmx/UpdateChecker.cs
@@ -1,13 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Reflection;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using System.Net.Http;
-using System.IO;
 using System.Runtime.InteropServices;
 
 namespace D2L.Bmx {
