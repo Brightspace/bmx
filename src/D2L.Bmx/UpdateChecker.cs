e8ed0dc381dd1e3537e96a4994d0322bf6ef4739
Liam Gordon <liam.gordon@d2l.com>
2023-09-07 09:41:10 -0400

    misaligned

--- src/D2L.Bmx/UpdateChecker.cs
+++ src/D2L.Bmx/UpdateChecker.cs
@@ -4,42 +4,51 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace D2L.Bmx;
-internal static class UpdateChecker {
+internal static class UpdateChecker
+{
 
-	public static async Task CheckForUpdatesAsync( BmxConfig config ) {
-		try {
+	public static async Task CheckForUpdatesAsync(BmxConfig config)
+	{
+		try
+		{
 			var cachedVersion = GetUpdateCheckCache();
 			var localVersion = Assembly.GetExecutingAssembly().GetName().Version;
-			var latestVersion = new Version( cachedVersion?.VersionName ?? "0.0.0" );
-			if( ShouldFetchLatestVersion( cachedVersion ) ) {
-				latestVersion = new Version( await GetLatestReleaseVersionAsync() );
+			var latestVersion = new Version(cachedVersion?.VersionName ?? "0.0.0");
+			if (ShouldFetchLatestVersion(cachedVersion))
+			{
+				latestVersion = new Version(await GetLatestReleaseVersionAsync());
 			}
 
-			string updateLocation = string.Equals( config.Org, "d2l", StringComparison.OrdinalIgnoreCase )
+			string updateLocation = string.Equals(config.Org, "d2l", StringComparison.OrdinalIgnoreCase)
 				? "https://bmx.d2l.dev"
 				: "https://github.com/Brightspace/bmx/releases/latest";
 
-			if( latestVersion > localVersion ) {
-				DisplayUpdateMessage( $"A new BMX release is available: v{latestVersion} (current: v{localVersion})\n" +
-					$"Upgrade now at {updateLocation}" );
+			if (latestVersion > localVersion)
+			{
+				DisplayUpdateMessage($"A new BMX release is available: v{latestVersion} (current: v{localVersion})\n" +
+					$"Upgrade now at {updateLocation}");
 			}
-		} catch( Exception ) {
+		}
+		catch (Exception)
+		{
 			// Give up and don't bother telling the user we couldn't check for updates
 		}
 	}
 
-	private static void DisplayUpdateMessage( string message ) {
+	private static void DisplayUpdateMessage(string message)
+	{
 		var originalBackgroundColor = Console.BackgroundColor;
 		var originalForegroundColor = Console.ForegroundColor;
 
 		Console.BackgroundColor = ConsoleColor.Gray;
 		Console.ForegroundColor = ConsoleColor.Black;
 
-		string[] lines = message.Split( "\n" );
+		string[] lines = message.Split("\n");
 		int consoleWidth = Console.WindowWidth;
 
-		foreach( string line in lines ) {
-			Console.Error.Write( line.PadRight( consoleWidth ) );
+		foreach (string line in lines)
+		{
+			Console.Error.Write(line.PadRight(consoleWidth));
 			Console.Error.WriteLine();
 		}
 
@@ -49,12 +58,13 @@ internal static class UpdateChecker {
 		Console.Error.WriteLine();
 	}
 
-	private static async Task<string> GetLatestReleaseVersionAsync() {
+	private static async Task<string> GetLatestReleaseVersionAsync()
+	{
 		using var httpClient = new HttpClient();
-		httpClient.BaseAddress = new Uri( "https://api.github.com" );
-		httpClient.Timeout = TimeSpan.FromSeconds( 5 );
-		httpClient.DefaultRequestHeaders.Add( "User-Agent", "BMX" );
-		var response = await httpClient.GetAsync( "repos/Brightspace/bmx/releases/latest" );
+		httpClient.BaseAddress = new Uri("https://api.github.com");
+		httpClient.Timeout = TimeSpan.FromSeconds(5);
+		httpClient.DefaultRequestHeaders.Add("User-Agent", "BMX");
+		var response = await httpClient.GetAsync("repos/Brightspace/bmx/releases/latest");
 		response.EnsureSuccessStatusCode();
 
 		await using var responseStream = await response.Content.ReadAsStreamAsync();
@@ -62,65 +72,80 @@ internal static class UpdateChecker {
 			responseStream,
 			SourceGenerationContext.Default.GithubRelease
 		);
-		string version = releaseData?.TagName?.Replace( "v", "" ) ?? string.Empty;
-		SaveLatestVersion( version );
+		string version = releaseData?.TagName?.Replace("v", "") ?? string.Empty;
+		SaveLatestVersion(version);
 		return version;
 	}
 
-	private static void SaveLatestVersion( string version ) {
-		if( !Directory.Exists( BmxPaths.BMX_DIR ) ) {
-			Directory.CreateDirectory( BmxPaths.BMX_DIR );
+	private static void SaveLatestVersion(string version)
+	{
+		if (!Directory.Exists(BmxPaths.BMX_DIR))
+		{
+			Directory.CreateDirectory(BmxPaths.BMX_DIR);
 		}
-		if( string.IsNullOrWhiteSpace( version ) ) {
+		if (string.IsNullOrWhiteSpace(version))
+		{
 			return;
 		}
-		var cache = new UpdateCheckCache {
+		var cache = new UpdateCheckCache
+		{
 			VersionName = version,
 			TimeLastChecked = DateTimeOffset.UtcNow
 		};
-		string content = JsonSerializer.Serialize( cache, SourceGenerationContext.Default.UpdateCheckCache );
-		var op = new FileStreamOptions {
+		string content = JsonSerializer.Serialize(cache, SourceGenerationContext.Default.UpdateCheckCache);
+		var op = new FileStreamOptions
+		{
 			Mode = FileMode.OpenOrCreate,
 			Access = FileAccess.ReadWrite,
 		};
-		if( !RuntimeInformation.IsOSPlatform( OSPlatform.Windows ) ) {
+		if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+		{
 			op.UnixCreateMode = UnixFileMode.UserRead | UnixFileMode.UserWrite;
 		}
-		using var fs = new FileStream( BmxPaths.UPDATE_CHECK_FILE_NAME, op );
-		using var writer = new StreamWriter( fs );
-		writer.Write( content );
+		using var fs = new FileStream(BmxPaths.UPDATE_CHECK_FILE_NAME, op);
+		using var writer = new StreamWriter(fs);
+		writer.Write(content);
 	}
 
-	private static UpdateCheckCache? GetUpdateCheckCache() {
-		if( !File.Exists( BmxPaths.UPDATE_CHECK_FILE_NAME ) ) {
+	private static UpdateCheckCache? GetUpdateCheckCache()
+	{
+		if (!File.Exists(BmxPaths.UPDATE_CHECK_FILE_NAME))
+		{
 			return null;
 		}
 
-		string content = File.ReadAllText( BmxPaths.UPDATE_CHECK_FILE_NAME );
-		try {
-			return JsonSerializer.Deserialize( content, SourceGenerationContext.Default.UpdateCheckCache );
-		} catch( JsonException ) {
+		string content = File.ReadAllText(BmxPaths.UPDATE_CHECK_FILE_NAME);
+		try
+		{
+			return JsonSerializer.Deserialize(content, SourceGenerationContext.Default.UpdateCheckCache);
+		}
+		catch (JsonException)
+		{
 			return null;
 		}
 	}
 
-	private static bool ShouldFetchLatestVersion( UpdateCheckCache? cache ) {
-		if( cache is null || string.IsNullOrWhiteSpace( cache.VersionName )
-			|| ( DateTimeOffset.UtcNow - cache.TimeLastChecked ) > TimeSpan.FromDays( 1 )
-			|| ( cache.TimeLastChecked > DateTimeOffset.UtcNow )
-		 ) {
+	private static bool ShouldFetchLatestVersion(UpdateCheckCache? cache)
+	{
+		if (cache is null || string.IsNullOrWhiteSpace(cache.VersionName)
+			|| (DateTimeOffset.UtcNow - cache.TimeLastChecked) > TimeSpan.FromDays(1)
+			|| (cache.TimeLastChecked > DateTimeOffset.UtcNow)
+		)
+		{
 			return true;
 		}
 		return false;
 	}
 }
 
-internal record GithubRelease {
-	[JsonPropertyName( "tag_name" )]
+internal record GithubRelease
+{
+	[JsonPropertyName("tag_name")]
 	public string? TagName { get; set; }
 }
 
-internal record UpdateCheckCache {
+internal record UpdateCheckCache
+{
 	public string? VersionName { get; set; }
 	public DateTimeOffset? TimeLastChecked { get; set; }
 }
