a494d895faff40fa5fa441e8519f2cce72c59919
gord5500 <90227099+gord5500@users.noreply.github.com>
2024-06-19 09:38:06 -0400

    write: update config file section with same name if no cred process flag (#453)
    ### Why
    
    Default credentials file takes priority over the default config which is
    where we store profiles using `credential_process`. If someone had an
    existing profile in config file and then runs `bmx write`, creds will be
    stored into the default credentials file making the existing one
    useless. We should remove the existing key in the config file so there
    is no confusion.
    
    `credential_process` is the better way for majority of people but there
    are some scenarios where it's not supported. So we are leaving some
    information to the user that it is no longer being used. Just in case
    they unintentionally ran write without the flag. Not going with a prompt
    to confirm because that could break existing scripts
    
    ### Ticket
    
    VUL-385
    
    ---------
    
    Co-authored-by: Chenfeng Bao <chenfeng.bao@d2l.com>

--- src/D2L.Bmx/WriteHandler.cs
+++ src/D2L.Bmx/WriteHandler.cs
@@ -85,6 +85,27 @@ internal class WriteHandler(
 				+ $" --role {awsCredsInfo.Role}"
 				+ $" --duration {awsCredsInfo.Duration}";
 		} else {
+			if( File.Exists( SharedCredentialsFile.DefaultConfigFilePath ) ) {
+				string sectionName = $"profile {profile}";
+				var defaultConfigFile = parser.ReadFile( SharedCredentialsFile.DefaultConfigFilePath );
+				if( defaultConfigFile.Sections.ContainsSection( sectionName )
+					&& defaultConfigFile[sectionName].ContainsKey( "credential_process" ) ) {
+
+					if( defaultConfigFile[sectionName].Count == 1 ) {
+						defaultConfigFile.Sections.RemoveSection( sectionName );
+					} else {
+						defaultConfigFile[sectionName].RemoveKey( "credential_process" );
+					}
+					parser.WriteFile( SharedCredentialsFile.DefaultConfigFilePath, defaultConfigFile );
+					Console.WriteLine(
+"""
+An existing profile with the same name using the `credential_process` setting was found in the default config file.
+The setting has been removed, and static credentials will be used for the profile.
+To continue using non-static credentials, rerun the command with the --use-credential-process flag.
+"""
+					);
+				}
+			}
 			if( !data.Sections.ContainsSection( profile ) ) {
 				data.Sections.AddSection( profile );
 			}
