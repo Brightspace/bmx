5d9989f80d2a81b4dd9af7d2e7bbdeb442e2b787
Liam Gordon <lgordon@desire2learn.com>
2026-06-03 14:04:46 -0400

    bump max to 60 and increase timeouts based on set value

--- src/D2L.Bmx/BmxConfig.cs
+++ src/D2L.Bmx/BmxConfig.cs
@@ -3,7 +3,7 @@ namespace D2L.Bmx;
 internal static class PasswordlessTimeoutDefaults {
 	public const int Min = 5;
 	public const int Max = 30;
-	public const int Default = 30;
+	public const int Default = 60;
 }
 
 internal record BmxConfig(
